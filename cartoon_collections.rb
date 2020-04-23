3d75ea8d02dd6d744405138e36b720e3b0f6327b
Steve Womble <swomble@aol.com>
2020-04-23 18:26:51 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,6 +13,7 @@ def long_planeteer_calls(calls)
 end
 
 def find_the_cheese(array)
+  array.find{|i|
   cheese_types = ["cheddar", "gouda", "camembert"]
-   if strings.include?cheese_types
+  
 end
