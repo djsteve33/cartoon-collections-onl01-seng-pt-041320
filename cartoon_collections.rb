4681bea6521f194b55841e91c64aaed3100e03e8
Steve Womble <swomble@aol.com>
2020-04-23 18:17:08 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,7 +8,7 @@ def summon_captain_planet(array)
   array.map {|i| i.capitalize << "!"}
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(array)
   # Your code here
 end
 
